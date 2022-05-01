6e7b06f2300b2c8ee8d354bf53054bad705ae29c
JeremyColegrove2U <48567475+JeremyMColegrove@users.noreply.github.com>
2022-05-01 13:31:41 -0500

    Added useDispatch and useSelector

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,28 +1,8 @@
 import React, { createContext, useContext } from "react";
 import { reducer } from './reducers'
 
-import {createStore} from 'react-redux'
+import {createStore} from 'redux'
 
 export const store = createStore(reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-// const StoreContext = createContext();
-// const { Provider } = StoreContext;
-
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useProductReducer({
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: '',
-//   });
-
-//   return <Provider value={[state, dispatch]} {...props} />;
-// };
-
-// const useStoreContext = () => {
-//   return useContext(StoreContext);
-// };
-
-// export { StoreProvider, useStoreContext };
